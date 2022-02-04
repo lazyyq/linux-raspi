98a5ee1bc1d0484ce701c66af987c8e362c141fd
lazyyq <kykint@naver.com>
2022-02-04 22:27:03 +0900

    Merge kernel-5.14-backport.patch from AUR ntfs3-dkms-git
    https://aur.archlinux.org/packages/ntfs3-dkms-git

--- fs/ntfs3/file.c
+++ fs/ntfs3/file.c
@@ -1034,10 +1034,10 @@ static ssize_t ntfs_compress_write(struct kiocb *iocb, struct iov_iter *from)
 			size_t cp, tail = PAGE_SIZE - off;
 
 			page = pages[ip];
-			cp = copy_page_from_iter_atomic(page, off,
-							min(tail, bytes), from);
+			cp = iov_iter_copy_from_user_atomic(page, from, off,
+							    min(tail, bytes));
 			flush_dcache_page(page);
-
+			iov_iter_advance(from, cp);
 			copied += cp;
 			bytes -= cp;
 			if (!bytes || !cp)
